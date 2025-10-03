111f5e2bc1b4b73a8d0058532201e0c34ad0e5f0
vikasrathod4299 <vikas.rv4299@gmail.com>
2025-10-03 17:54:27 +0530

    feat: add spinning animation for Spotify icon when a track is playing; define animation in CSS

--- src/components/Home/Profile/SpotifyNowPlaying.tsx
+++ src/components/Home/Profile/SpotifyNowPlaying.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 
 export default function SpotifyNowPlaying() {
     const [nowPlaying, setNowPlaying] = useState<NowPlaying>();
+    console.log(nowPlaying)
 
     const getNowPlaying = async () => {
         const { data } = await axios.get("/now-playing");
@@ -20,7 +21,7 @@ export default function SpotifyNowPlaying() {
     return (
         <div className="flex items-center gap-3 xl:ml-auto">
             <a href="https://open.spotify.com/user/31m4l8kqz4k1b2wqv6xg4n3y5e" target="_blank" rel="noopener noreferrer">
-                <IconBrandSpotify color="#1DB954" />
+                <IconBrandSpotify color="#1DB954"  className={nowPlaying?.isPlaying ? "animate-spin-slow" : ""} />
             </a>
             <span className="text-sm font-semibold text-black dark:text-white sm:text-base">
             {!nowPlaying || !nowPlaying.isPlaying ? "Not Playing": nowPlaying.name.length > 30 ? nowPlaying.name.slice(0, 30) + "..." : nowPlaying.name}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,56 +8,5 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}'
   ],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        primary: colors.sky
-      },
-      animation: {
-        'scale-in-right':
-          'scale-in-right 0.2s cubic-bezier(0.250, 0.460, 0.450, 0.940)  both',
-        'scale-in-right-reverse':
-          'scale-in-right 0.2s cubic-bezier(0.250, 0.460, 0.450, 0.940)   reverse both'
-      },
-      keyframes: {
-        'scale-in-right': {
-          '0%': {
-            transform: 'scale(0)',
-            'transform-origin': '100% 50%',
-            opacity: '1'
-          },
-          to: {
-            transform: 'scale(1)',
-            'transform-origin': '100% 50%',
-            opacity: '1'
-          }
-        },
-        'scale-in-right-reverse': {
-          '0%': {
-            transform: 'scale(0)',
-            'transform-origin': '100% 50%',
-            opacity: '1'
-          },
-          to: {
-            transform: 'scale(1)',
-            'transform-origin': '100% 50%',
-            opacity: '1'
-          }
-        }
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            pre: null
-          }
-        }
-      }
-    },
-    fontFamily: {
-      sans: ['Nunito', 'sans-serif'],
-      mono: ['JetBrains Mono', 'monospace']
-    }
-  },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
 };
