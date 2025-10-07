fc3190cfda0fe79158ed74227d8eeb22fead2bd3
vikasrathod4299 <vikas.rv4299@gmail.com>
2025-10-08 00:52:26 +0530

    fix: remove unnecessary margin from NotionRenderer component

--- src/components/Blogs/Notion/NotinoRenderer.tsx
+++ src/components/Blogs/Notion/NotinoRenderer.tsx
@@ -39,7 +39,7 @@ const NotionRenderer = ({ blocks }: NotionRendererProps) => {
   });
 
   return (
-    <div className="max-w-3xl mx-auto p-6 font-sans text-white">
+    <div className="max-w-3xl p-6 font-sans text-white">
       {groupedBlocks.map((item, index) => {
         if (item.type === 'bulleted') {
           return (
